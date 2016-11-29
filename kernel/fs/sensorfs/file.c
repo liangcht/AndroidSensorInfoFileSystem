aecda47b86cefb188da665a4e04bd4054936a228
Bo Gan <gan.bo@columbia.edu>
2016-11-29 12:25:26 -0500

    sensorfs

---
+++ kernel/fs/sensorfs/file.c
@@ -0,0 +1,25 @@
+/* file.c: sensorfs file implementation
+ *
+ * W4118 Homework 6
+ */
+
+#include <linux/fs.h>
+#include <linux/mm.h>
+#include <linux/sensorfs.h>
+
+#include "internal.h"
+
+//TODO: Probably need a few function implemented here
+//and to fill out the following structs appropriately.
+
+const struct file_operations sensorfs_file_operations = {
+};
+
+const struct inode_operations sensorfs_file_inode_operations = {
+};
+
+const struct file_operations sensorfs_dir_operations = {
+};
+
+static const struct inode_operations sensorfs_dir_inode_operations = {
+};
